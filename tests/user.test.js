8f9848ee8f4ef90dcc528918fb7f447e2c7a63c7
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2023-10-31 10:10:17 +0100

    edited user tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const app = require('../app')
 const User = require('../model/user')
+afterAll( async () => {await User.deleteMany()})
 test('Should signup a new user', async () => {
     const response = await request(app).post('/register')
         .send({
@@ -16,20 +17,18 @@ test('Should signup a new user', async () => {
     expect(user.password).not.toBe('1234567890')
 })
 
-
-test('Should login a new user', async () => {
-    const userData = {
-        email: "dhriti@test.com",
-        password: "1234567890"
-    };
-
-    const response = await request(app)
-        .post('/login')
-        .send(userData);
-
-    expect(response.status).toBe(200);
-    expect(response.body.token).toBeDefined();
-    const decoded = jwt.decode(response.body.token);
-    expect(decoded.userId).toBeDefined();
-    expect(response.body.message).toBe("Bonjour dhriti");
+describe('Login Endpoint', () => {
+    it('should log in a user with valid credentials', async () => {
+        const response = await request(app)
+            .post('/login')
+            .send({
+                email: 'dhriti@test.com',
+                password: '1234567890',
+            });
+        expect(response.status).toBe(200);
+        expect(response.body.token).toBeDefined();
+        const decoded = jwt.decode(response.body.token);
+        expect(decoded.userId).toBeDefined();
+        expect(response.body.message).toBe("Bonjour dhriti");
+    });
 });
